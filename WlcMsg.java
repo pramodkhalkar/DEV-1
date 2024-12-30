71ca2de8dd56a5b208c9e6ea517c050c9c9e21d9
pramodkhalkar <pramodkhalkar70@gmail.com>
2024-12-30 02:47:18 -0800

    Update WlcMsg.java

--- WlcMsg.java
+++ WlcMsg.java
@@ -1,5 +1,5 @@
 public class WlcMsg {
     public static void main(String[] args) {
-        System.out.println("Welcome to the CI/CD Pipeline Practical!");
+        System.out.println("wlcMsg");
     }
 }
